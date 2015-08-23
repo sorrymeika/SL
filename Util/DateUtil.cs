79acc95ed93f234624e29e44247e1ca1c2c55916
sorrymeika <sorrymeika@163.com>
2015-08-24 04:53:41 +0800

    fx

---
+++ Util/DateUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SL.Util
+{
+    public class DateUtil
+    {
+        public static int MonthDiff(DateTime start, DateTime end)
+        {
+            return end.Month + (end.Year - start.Year) * 12 - start.Month;
+        }
+    }
+
+}
